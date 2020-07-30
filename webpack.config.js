3cf438dca64dc4a2a5457156feeba9954b4c078e
Spencer Smith <ssmith@sofi.org>
2020-07-30 17:31:29 -0600

    Added build to docs

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const CopyPlugin = require('copy-webpack-plugin');
 module.exports = {
   entry: './src/app.js',
   output: {
-    path: __dirname + '/dist',
+    path: __dirname + '/docs',
     filename: 'js/bundle.js',
   },
   module: {
